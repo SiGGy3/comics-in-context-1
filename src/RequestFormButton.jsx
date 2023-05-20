14afd56fc985d8e8198c8426315ced2f55e415aa
Justin Sigarroa <justinjwsig@gmail.com>
2023-05-20 13:55:55 -0700

    added imports using react

--- src/RequestFormButton.jsx
+++ src/RequestFormButton.jsx
@@ -1,10 +1,14 @@
 import React, { useContext } from "react";
+import StyleContext from "./StyleContext";
 
 
 export default function RequestFormButton() {
+
+  const buttonStyling = useContext(StyleContext);
+
   return (
     <div>
-      <button>
+      <button style={buttonStyling}>
         Click Here!
       </button>
       <input type="submit" value="Submit" />
